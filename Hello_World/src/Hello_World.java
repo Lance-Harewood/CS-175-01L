b91763ee37b7c3982fcd71e5411baf50cbffebdc
s1291523 <s1291523@cssemaclin24.monmouth.edu>
2020-02-03 12:51:24 -0500

    House Painting commit

--- Hello_World/src/Hello_World.java
+++ Hello_World/src/Hello_World.java
@@ -2,10 +2,52 @@
 public class Hello_World {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// One line comment
 		
-		System.out.println("Lance Harewood!");
+		/* This is a
+		 * very long 
+		  comment */
 		
+		int i = 12, j = 5, k = -3; double x = 2.1, y = -1.5, z = 3.0;
+		int expr1=(i+j+k)/3;
+		System.out.println(expr1);
+		int expr2=(i/j) * j + (i%j);
+		System.out.println(expr2);
+		double expr3= x*x+y*y;
+		System.out.println(expr3);
+		double expr4=(x+y+z)/(x-y-z);
+		System.out.println(expr4);
+		double expr5=2.0*z-(x+y);
+		System.out.println(expr5);
+		
+		
+//		int dollars=1;
+//		int quarters=2;
+//		int dimes=3;
+//		int nickels=4;
+//		int pennies=5;
+//		
+//		final double QUARTER_VALUE=0.25;
+//		final double DIME_VALUE=0.1;
+//		final double NICKEL_VALUE=0.05;
+//		final double PENNY_VALUE=0.01;
+//		
+//		double payment=dollars*1 + quarters*QUARTER_VALUE + dimes*DIME_VALUE + nickels*NICKEL_VALUE+ pennies*PENNY_VALUE;
+//		System.out.print(payment);
+		
+//		long width=1_000_000_000;
+//		System.out.println(width*width);
+//		byte byteInt=100;
+//		short shortInt=1000;
+//		long longInt=100000000;
+//		System.out.println("int type: ");
+//		System.out.println(width);
+//		System.out.println("byte type: ");
+//		System.out.println(byteInt);
+//		System.out.println("short type: ");
+//		System.out.println(shortInt);
+//		System.out.println("long type: ");
+//		System.out.println(longInt);
 		
 	}
 	
